b707f6a4f3315534d6b3845d286b7ebfc142b662
Lakshmikanthan <lakshmikanthan.babu@xtract.io>
2025-06-05 11:23:54 +0530

    Initial portfolio commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PortfolioComponentComponent } from "./portfolio-component/portfolio-component.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PortfolioComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
